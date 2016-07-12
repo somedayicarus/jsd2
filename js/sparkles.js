eee5cd5185a3ae1e5a56c64279a4e377ccc816f1
Niki Ng <nicole.therese.ng@gmail.com>
2016-07-12 16:28:01 -0700

    added sparkles.js

---
+++ js/sparkles.js
@@ -0,0 +1,5 @@
+//create variable and link it to sparkle button
+var sparkleButton = document.getElementById("sparkle-btn");
+
+//add event listener
+sparkleButton.addEventListener('click', addSparkles)
