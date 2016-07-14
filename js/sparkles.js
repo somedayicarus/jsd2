0a09e12e0febde92d545f120e8dde00e80113bba
Niki Ng <nicole.therese.ng@gmail.com>
2016-07-14 12:20:49 -0700

    added sparkle function

--- js/sparkles.js
+++ js/sparkles.js
@@ -1,5 +1,36 @@
+//array of sparkle gifs
+var bling = ['images/sparkle_yellow.gif', 'images/silver_stars.gif',
+ 'images/tiny_blue_sparkle.gif', 'images/large_star_outline_pink.gif',
+ 'images/pink_sparkle_stars.gif', 'images/sparkle_green.gif', 'images/sparkles.gif',
+ 'images/sparkle_silver.gif' ]
+
+
 //create variable and link it to sparkle button
 var sparkleButton = document.getElementById("sparkle-btn");
 
 //add event listener
 sparkleButton.addEventListener('click', addSparkles)
+
+
+//addSparkles function 
+function addSparkles() {
+	//generate random sparkle gif
+	var randomSparkle =  bling[getRandomInt(0, bling.length - 1)];
+
+	//generate random x and y axis
+	var randomX = getRandomInt(0, 100);
+	var randomY = getRandomInt(0, 100);
+
+	var sparkleGIF = document.createElement("img");
+	sparkleGIF.src = randomSparkle;
+	sparkleGIF.style.position = "absolute";
+	sparkleGIF.style.top = randomX + "%";
+	sparkleGIF.style.left = randomY + "%";
+	document.getElementById('container').appendChild(sparkleGIF);
+
+}
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
