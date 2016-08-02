59750dc5822f00b105812a7127ac75cddf0a7db0
Niki Ng <nicole.therese.ng@gmail.com>
2016-08-01 17:52:50 -0700

    started traffic light hw

---
+++ homework/traffic-light/js/trafficLight.js
@@ -0,0 +1,35 @@
+
+// Structure
+// ----------------------------------------------
+var stop = document.querySelector(".stop-button");
+var slow = document.querySelector(".slow-button");
+var go = document.querySelector(".go-button");
+var caution = document.querySelector(".caution-button");
+var stopLight = document.querySelector(".stop-light");
+
+// Events
+// ----------------------------------------------
+stop.addEventListener("click", redLight);
+slow.addEventListener("click", yellowLight);
+go.addEventListener("click", greenLight);
+caution.addEventListener("click", blinkingLight);
+
+
+// Event handlers
+// ----------------------------------------------
+function redLight(e) {
+
+
+};
+
+function yellowLight(e) {
+
+};
+
+function greenLight(e) {
+
+};
+
+function blinkingLight(e) {
+
+};
