8140f88f1894f0bb9bf7431f19882d5d5e8b4de9
somedayicarus <niki.t.noll@gmail.com>
2016-08-01 21:21:45 -0700

    create li elements

--- exercises/ajax/js/ajax.js
+++ exercises/ajax/js/ajax.js
@@ -1,5 +1,6 @@
 
 var p = document.querySelector("p");
+var ul = document.querySelector(".concepts");
 
 // api request w/ jquery
 var consumerFinanceAPI = "https://api.consumerfinance.gov/data/hmda.json";
@@ -8,6 +9,14 @@ var jqxhr = $.getJSON(consumerFinanceAPI, handleData);
 
 function handleData(json) {
 	console.log(json);
-	var description = json["description"];
+	var description = json["description"];	
 	p.innerHTML = description;
+
+	json["_embedded"]["concepts"].forEach(createConcepts);
+
+	function createConcepts(item) {
+		var li = document.createElement("li");
+		li.innerHTML = item["description"];
+		ul.appendChild(li);
+	}
 };
