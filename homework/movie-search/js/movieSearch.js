e279e24f37921bf0918a51c5270e25579698e932
Niki Ng <nicole.therese.ng@gmail.com>
2016-08-05 15:02:52 -0700

    initial commit

---
+++ homework/movie-search/js/movieSearch.js
@@ -0,0 +1,53 @@
+// Structure
+// ----------------------------------------------
+var results = document.querySelector(".results")
+var search = document.querySelector("#query");
+var form = document.querySelector("form");
+var img = document.querySelector(".poster")
+var OMDbAPI;
+
+// Events
+// ----------------------------------------------
+form.addEventListener("submit", getData);
+
+// Event handlers
+// ----------------------------------------------
+function getData(e) {
+	e.preventDefault();
+	console.log(e);
+	OMDbAPI = "http://www.omdbapi.com/?s=" + search.value;;
+	$.getJSON(OMDbAPI, handleData);
+}
+
+function handleData(json) {
+	//clear previous search results
+	results.innerHTML = "";
+	img.src = "";
+	json["Search"].forEach(listResults);
+}
+
+function listResults(movie) {
+	//create elements
+	var li = document.createElement("li");
+	var a = document.createElement("a");
+	var aText = document.createTextNode(movie["Title"]);
+
+	//add content
+	a.appendChild(aText);
+	a.href = movie["Poster"];
+
+	//update page
+	li.appendChild(a);
+	results.appendChild(li);
+
+	//display poster on click
+	a.addEventListener("click", function(e) {
+		e.preventDefault();
+		if(movie["Poster"] === "N/A") {
+			img.src = "images/no-poster.png"
+		} else {
+			img.src = a.href;
+		}
+	})
+}
+
