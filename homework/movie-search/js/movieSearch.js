e467d8688a28013a63fdd3d20c86897bd61346fc
Niki Ng <nicole.therese.ng@gmail.com>
2016-08-08 16:41:37 -0700

    cleaned up js

--- homework/movie-search/js/movieSearch.js
+++ homework/movie-search/js/movieSearch.js
@@ -30,8 +30,10 @@ function getMovies(e) {
 	$.getJSON(OMDbAPI, displayResults);
 };
 
+
 //interate over json array and call listResults on each object
 function displayResults(json) {
+
 	//clear previous search results
 	results.innerHTML = "";
 	poster.innerHTML = "";
@@ -53,8 +55,10 @@ function displayResults(json) {
 	});
 };
 
+
 //when passed movie object - create list items with each movies properties
 function listResults(movie) {
+
 	//create elements
 	var li = document.createElement("li");
 	var img = document.createElement("img");
@@ -74,7 +78,7 @@ function listResults(movie) {
 	li.appendChild(img);
 	li.appendChild(p);
 	results.appendChild(li);
-}
+};
 
 
 
@@ -87,7 +91,7 @@ function getDetails(e) {
 
 	poster.innerHTML = "";
 	details.innerHTML = "";
-}
+};
 
 
 //when passed movie detail object - create elements
@@ -96,33 +100,34 @@ function listDetail(json) {
 	if(json["Response"] === "False") {
 		console.log(json["Error"]);
 	} else {
-	//create elements
-	var img = document.createElement("img");
-	var h2 = document.createElement("h2");
-	var p1 = document.createElement("p");
-	var p2 = document.createElement("p");
-	var a = document.createElement("a")
-	var aText = document.createTextNode("View on IMDb");
 
-	//add content
-	a.appendChild(aText);
-	h2.textContent = json["Title"];
-	a.target = "_blank";
-	a.href = "http://www.imdb.com/title/" + json["imdbID"];
-	p1.textContent = json["Plot"];
+		//create elements
+		var img = document.createElement("img");
+		var h2 = document.createElement("h2");
+		var p1 = document.createElement("p");
+		var p2 = document.createElement("p");
+		var a = document.createElement("a")
+		var aText = document.createTextNode("View on IMDb");
 
-	if(json["Poster"] === "N/A") {
-		img.src = posterNotFound;
-	} else {
-		img.src = json["Poster"];
-	}
+		//add content
+		a.appendChild(aText);
+		h2.textContent = json["Title"];
+		a.target = "_blank";
+		a.href = "http://www.imdb.com/title/" + json["imdbID"];
+		p1.textContent = json["Plot"];
 
-	//update page
-	poster.appendChild(img);
-	details.appendChild(h2);
-	details.appendChild(p1);
-	p2.appendChild(a);
-	details.appendChild(p2);
+		if(json["Poster"] === "N/A") {
+			img.src = posterNotFound;
+		} else {
+			img.src = json["Poster"];
+		}
+
+		//update page
+		poster.appendChild(img);
+		details.appendChild(h2);
+		details.appendChild(p1);
+		p2.appendChild(a);
+		details.appendChild(p2);
 	}
 };
 
